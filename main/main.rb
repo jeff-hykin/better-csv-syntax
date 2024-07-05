105e71cd649e818b40e0ff012e7b295a71c2e103
Jeff Hykin <jeff.hykin@gmail.com>
2024-07-05 14:51:15 -0500

    setup lang for debugging

--- main/main.rb
+++ main/main.rb
@@ -12,7 +12,7 @@ require_relative './tokens.rb'
 # grammar = Grammar.fromTmLanguage("./original.tmLanguage.json")
 grammar = Grammar.new(
     name: "CSV",
-    scope_name: "source.csv",
+    scope_name: "text.csv",
     fileTypes: [
         "csv",
         # for example here are come C++ file extensions:
@@ -23,16 +23,40 @@ grammar = Grammar.new(
     version: "",
 )
 
+{
+    "name": "csv syntax",
+    "scopeName": "text.csv",
+    "fileTypes": ["csv"],
+    "patterns": [
+        {
+            "match": "((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?((?: *\"(?:[^\"]*\"\")*[^\"]*\" *(?:,|$))|(?:[^,]*(?:,|$)))?",
+            "name": "rainbowgroup",
+            "captures": {
+                "1": {"name": "rainbow1"},
+                "2": {"name": "keyword.rainbow2"},
+                "3": {"name": "entity.name.function.rainbow3"},
+                "4": {"name": "comment.rainbow4"},
+                "5": {"name": "string.rainbow5"},
+                "6": {"name": "variable.parameter.rainbow6"},
+                "7": {"name": "constant.numeric.rainbow7"},
+                "8": {"name": "entity.name.type.rainbow8"},
+                "9": {"name": "markup.bold.rainbow9"},
+                "10": {"name": "invalid.rainbow10"}
+            }
+        }
+
+    ],
+    "uuid": "ca03e352-04ef-4340-9a6b-9b99aae1c418"
+}
+
+
 # 
 #
 # Setup Grammar
 #
 # 
     grammar[:$initial_context] = [
-        :comments,
-        :string,
-        :variable,
-        # (add more stuff here) (variables, strings, numbers)
+        :item,
     ]
 
 # 
@@ -61,38 +85,71 @@ grammar = Grammar.new(
 # 
 # basic patterns
 # 
-    grammar[:variable] = Pattern.new(
-        match: variable,
-        tag_as: "variable.other",
+    grammar[:escape] = Pattern.new(
+        match: /""/,
+        tag_as: "constant.character.escape",
     )
-    
-    grammar[:line_continuation_character] = Pattern.new(
-        match: /\\\n/,
-        tag_as: "constant.character.escape.line-continuation",
-    )
-    
-    grammar[:attribute] = PatternRange.new(
-        start_pattern: Pattern.new(
-                match: /\[\[/,
-                tag_as: "punctuation.section.attribute.begin"
-            ),
-        end_pattern: Pattern.new(
-                match: /\]\]/,
-                tag_as: "punctuation.section.attribute.end",
-            ),
-        tag_as: "support.other.attribute",
-        # tag_content_as: "support.other.attribute", # <- alternative that doesnt double-tag the start/end
-        includes: [
-            :attributes_context,
-        ]
-    )
-
-# 
-# imports
-# 
-    grammar.import(PathFor[:pattern]["comments"])
-    grammar.import(PathFor[:pattern]["string"])
-    grammar.import(PathFor[:pattern]["numeric_literal"])
+    separator = ","
+    generateRow = ->(column_tag:"entity.name.tag", column_number:1, children:[]) do
+        PatternRange.new(
+            start_pattern: Pattern.new(/(?:\G|\A)/),
+            end_pattern: Pattern.new(
+                tag_as: "punctuation.definition.entry",
+                match: separator,
+            ).or("\n"),
+            apply_end_pattern_last: true,
+            includes: [
+                # 
+                # one entry
+                # 
+                Pattern.new( # simple pattern
+                    match: /(?:\G|\A)[^"#{separator}]*(?=#{separator}|\n)/,
+                    tag_as: "string.unquoted #{column_tag} csv.column#{column_number}"
+                ),
+                PatternRange.new(
+                    tag_as: "string.quoted.double #{column_tag} csv.column#{column_number}",
+                    start_pattern: Pattern.new(
+                        /(?:\G|\A)/,
+                    ).then(
+                        tag_as: "punctuation.definition.string",
+                        match: '"',
+                    ),
+                    apply_end_pattern_last: true,
+                    end_pattern: Pattern.new(
+                        tag_as: "punctuation.definition.string",
+                        match: '"',
+                    ),
+                    includes: [
+                        :escape,
+                    ],
+                ),
+                # 
+                # subsequent entries
+                # 
+                *children,
+            ],
+        )
+    end
+    grammar[:item] = generateRow[column_tag:"rainbow1", column_number:1, children:[
+        generateRow[column_tag:"keyword.rainbow2", column_number:2, children:[
+            generateRow[column_tag:"entity.name.function.rainbow3", column_number:3, children:[
+                generateRow[column_tag:"comment.rainbow4", column_number:4, children:[
+                    generateRow[column_tag:"string.rainbow5", column_number:5, children:[
+                        generateRow[column_tag:"variable.parameter.rainbow6", column_number:6, children:[
+                            generateRow[column_tag:"constant.numeric.rainbow7", column_number:7, children:[
+                                generateRow[column_tag:"entity.name.type.rainbow8", column_number:8, children:[
+                                    generateRow[column_tag:"markup.bold.rainbow9", column_number:9, children:[
+                                        generateRow[column_tag:"invalid.rainbow10", column_number:10, children:[
+                                        ]]
+                                    ]]
+                                ]]
+                            ]]
+                        ]]
+                    ]]
+                ]]
+            ]]
+        ]]
+    ]]
 
 #
 # Save
